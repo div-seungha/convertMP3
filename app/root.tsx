21ad6593428495cc7acdd40b9f40f0847281bad0
seungha <sh.kim.x928@gmail.com>
2025-02-28 19:34:28 +0900

    Add: meta tag information

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,34 @@
-import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import { Links, Meta, MetaFunction, Outlet, Scripts } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Convert Youtube Clip to an Audio File" },
+    {
+      property: "og:title",
+      content: "Convert Youtube Clip to an Audio File",
+    },
+    {
+      property: "og:description",
+      content: "Convert Youtube Clip to an Audio File",
+    },
+    {
+      property: "og:image",
+      content: "/assets/background.jpg",
+    },
+    {
+      property: "og:image:width",
+      content: "1200",
+    },
+    {
+      property: "og:image:height",
+      content: "630",
+    },
+    {
+      name: "description",
+      content: "광고 일절 없는 유튜브 -> 오디오 컨버터",
+    },
+  ];
+};
 
 export default function App() {
   return (
