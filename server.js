f298290a2536af89e677864373522e1b96fb30f7
seungha <sh.kim.x928@gmail.com>
2025-03-15 02:29:13 +0900

    Add: cookie

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ app.post("/download-mp3", async (req, res) => {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
           "Accept-Language": "en-US,en;q=0.9",
+          cookie: process.env.COOKIE,
         },
       },
     };
