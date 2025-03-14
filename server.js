856b355082dc2e539517240ce7bfb902030fe6fe
seungha <sh.kim.x928@gmail.com>
2025-03-15 03:15:36 +0900

    [wip] Fix: deployment bugs

--- server.js
+++ server.js
@@ -43,11 +43,8 @@ app.post("/download-mp3", async (req, res) => {
     }
 
     const titleProcess = spawn("yt-dlp", [
-      "--force-ipv4",
-      "--user-agent",
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-      "--referer",
-      "https://www.youtube.com/",
+      "--extractor-args",
+      "youtube:player_client=web",
       "--get-title",
       url,
     ]);
